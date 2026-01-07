832dab0f5259f32883aca57df3283c037e5c2992
3fnpfncm <119072893+kay1mov@users.noreply.github.com>
2026-01-07 20:27:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from fastapi import FastAPI, Request
+import time
 
 app = FastAPI()
 
-storage = []
-
+storage_path = "storage"
 
 @app.post("/push")
 async def push(request: Request):
     data = await request.json()
-    storage.append(data)
-    return {"ok": True}
 
-
-@app.get("/get")
-async def get():
-    return {
-        "count": len(storage),
-        "data": storage
+    client_ip = request.client.host
+    print(client_ip)
+    full_data = {
+        "data": time.time(),
+        "client_ip": client_ip
     }
+
+    with open(storage_path + str(time.time()), "wb") as f:
+        json.dump(full_data, f, ensure_ascii=False, indent=4)
+
+    return {"ok": True}
 
 
 @app.get("/ping")
